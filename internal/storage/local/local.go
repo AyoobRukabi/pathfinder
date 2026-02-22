770e5b2ad8c33dfcd03621ef9e3fe70ee9ee7a4b
Ivan Andreev <ivan.joker88@gmail.com>
2026-02-22 22:33:17 +0200

    chore: add list of errors to check when validating map

--- internal/storage/local/local.go
+++ internal/storage/local/local.go
@@ -12,3 +12,17 @@ package local
 // If the file is broken, the repository should fail to build the object and return an error
 // (e.g., fmt.Errorf("invalid map: duplicate coordinates at line 10")).
 // The service layer should never have to worry if the map structurally makes sense.
+
+// Errors to check:
+// - It displays "Error" on stderr when the map does not contain a "stations:" section.
+// - It displays "Error" on stderr when the map does not contain a "connections:" section.
+// - It displays "Error" on stderr when the start station does not exist.
+// - It displays "Error" on stderr when the end station does not exist.
+// - It displays "Error" on stderr when no path exists between the start and end stations.
+// - It displays "Error" on stderr when duplicate routes exist between two stations, including in reverse.
+// - It displays "Error" on stderr when any of the coordinates are not valid positive integers.
+// - It displays "Error" on stderr when two stations exist at the same coordinates.
+// - It displays "Error" on stderr when a connection is made with a station which does not exist.
+// - It displays "Error" on stderr when station names are duplicated.
+// - It displays "Error" on stderr when station names are invalid.
+// - It displays "Error" on stderr when a map contains more than 10000 stations.
